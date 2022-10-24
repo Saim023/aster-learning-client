c260c12e334e07ede4972fea68da655a9c1c8255
saim-hossain <saimhossain023@gmail.com>
2022-10-24 21:49:24 +0600

    routes added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAwa-QwUkUcylxCNFRS5C0Afopcbi6MbLg",
+    authDomain: "aster-learning-e445d.firebaseapp.com",
+    projectId: "aster-learning-e445d",
+    storageBucket: "aster-learning-e445d.appspot.com",
+    messagingSenderId: "874265074396",
+    appId: "1:874265074396:web:2e9127f2663dd538f87219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
